22d16743c3ab0c46ad8005ef90fb98fd213f3063
caochanhduong <caochanhduongbku@gmail.com>
2020-02-12 07:50:10 +0700

    db

--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
 from dialogue_config import FAIL, SUCCESS
 
 
+
+
 def convert_list_to_dict(lst):
     """
     Convert list to dict where the keys are the list elements, and the values are the indices of the elements in the list.
